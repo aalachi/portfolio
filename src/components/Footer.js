432eae214102232c5cab5b9208f9505484a4768b
aalachi <mad.aalachi@gmail.com>
2026-01-10 19:25:25 +0100

    fix: update socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
     return(
         <div id="footer" style={{  width:'100vw' , backgroundColor:'white' , zIndex:'3' , color:'black' , minHeight:'10vh' }} >
                 <div className={styles.contact}><p>Contact me by <a href="mailto:mad.aalachi@gmail.com">mail</a>
-<span className={styles.social}><a target="_blank" href="http://www.github.com/a3lachi"><img src={Github} width={20} /></a>
+<span className={styles.social}><a target="_blank" href="http://www.github.com/aalachi"><img src={Github} width={20} /></a>
 <a target="_blank" href="https://www.linkedin.com/in/mohamed-aalachi-878479252/"><img src={Linkedin} width={20} /></a>
-<a target="_blank" href="https://www.youtube.com/channel/UC2A26hYnRVkNp1vn1eWSeCg"><img src={Youtube} width={20} /></a>
-<a target="_blank" href="https://twitter.com/a3laxi"><img src={Twitter} width={19} /></a></span>
+<a target="_blank" href="https://www.youtube.com/@aalachideployed"><img src={Youtube} width={20} /></a>
+<a target="_blank" href="https://x.com/aalachilogs"><img src={Twitter} width={19} /></a></span>
 <span className={styles.copyright}>© 2023 - 2025</span></p></div>
                 </div>
 
