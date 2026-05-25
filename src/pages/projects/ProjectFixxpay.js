079a13450cd3be6b3696df4c54f303a36a57da77
aalachi <mad.aalachi@gmail.com>
2026-05-25 17:25:27 +0100

    edit: omit swagger url in project showcase

--- src/pages/projects/ProjectFixxpay.js
+++ src/pages/projects/ProjectFixxpay.js
@@ -24,7 +24,7 @@ const ProjectFixxpay = () => {
                 <br></br>
                 <h2>Live URL</h2>
                 <p><a href="https://fixxpay.finance" target="_blank" style={{color:"black"}} rel="noopener noreferrer">https://fixxpay.finance</a></p>
-                <p>Swagger documentation is available at : <a href="https://fixxpay.finance/api/docs/" target="_blank" style={{color:"black"}} rel="noopener noreferrer">https://fixxpay.finance/api/docs/</a></p>
+                
                 <h2>Role</h2>
                 <p>Full Stack Engineer (Django & Python), I developed the entire platform from scratch, it includes the apps : authentication, users, blog, cart, comparison, contact us, orders, payments (Paypal and Caixa), products, reviews, search, newsletter and wishlist.</p>
                 <h2>The Challenge</h2>
